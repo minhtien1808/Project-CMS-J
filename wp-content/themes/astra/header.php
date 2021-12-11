d4786096e0ecb41c23b452e9cf2cf4c2332278f0
Minh Tiến <nguyenminhtien@example.com>
2021-12-11 08:54:44 +0700

    update

--- wp-content/themes/astra/header.php
+++ wp-content/themes/astra/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for Astra Theme.
  *
@@ -9,56 +10,52 @@
  * @package Astra
  * @since 1.0.0
  */
-
-if ( ! defined( 'ABSPATH' ) ) {
+//Luan
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <?php astra_html_before(); ?>
 <html <?php language_attributes(); ?>>
-<head>
-<?php astra_head_top(); ?>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="https://gmpg.org/xfn/11">
 
-<?php wp_head(); ?>
-<?php astra_head_bottom(); ?>
+<head>
+	<?php astra_head_top(); ?>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+	<?php astra_head_bottom(); ?>
 </head>
 
 <body <?php astra_schema_body(); ?> <?php body_class(); ?>>
-<?php astra_body_top(); ?>
-<?php wp_body_open(); ?>
+	<?php astra_body_top(); ?>
+	<?php wp_body_open(); ?>
 
-<a
-	class="skip-link screen-reader-text"
-	href="#content"
-	role="link"
-	title="<?php echo esc_html( astra_default_strings( 'string-header-skip-link', false ) ); ?>">
-		<?php echo esc_html( astra_default_strings( 'string-header-skip-link', false ) ); ?>
-</a>
+	<a class="skip-link screen-reader-text" href="#content" role="link" title="<?php echo esc_html(astra_default_strings('string-header-skip-link', false)); ?>">
+		<?php echo esc_html(astra_default_strings('string-header-skip-link', false)); ?>
+	</a>
 
-<div
-<?php
-	echo astra_attr(
-		'site',
-		array(
-			'id'    => 'page',
-			'class' => 'hfeed site',
-		)
-	);
-	?>
->
-	<?php
-	astra_header_before();
+	<div <?php
+			echo astra_attr(
+				'site',
+				array(
+					'id'    => 'page',
+					'class' => 'hfeed site',
+				)
+			);
+			?>>
+		<?php
+		astra_header_before();
 
-	astra_header();
+		astra_header();
 
-	astra_header_after();
+		astra_header_after();
 
-	astra_content_before();
-	?>
-	<div id="content" class="site-content">
-		<div class="ast-container">
-		<?php astra_content_top(); ?>
+		astra_content_before();
+		?>
+		<div id="content" class="site-content">
+			<div class="ast-container">
+				<?php astra_content_top(); ?>
