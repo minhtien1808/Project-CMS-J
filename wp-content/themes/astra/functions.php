6095f5673a9a4ac4f294d08a7f2a0ab62968b890
Minh Tiến <nguyenminhtien@example.com>
2021-12-11 08:57:26 +0700

    update

--- wp-content/themes/astra/functions.php
+++ wp-content/themes/astra/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Astra functions and definitions
  *
@@ -7,25 +8,25 @@
  * @package Astra
  * @since 1.0.0
  */
-
-if ( ! defined( 'ABSPATH' ) ) {
+//Thang
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
 /**
  * Define Constants
  */
-define( 'ASTRA_THEME_VERSION', '3.7.5' );
-define( 'ASTRA_THEME_SETTINGS', 'astra-settings' );
-define( 'ASTRA_THEME_DIR', trailingslashit( get_template_directory() ) );
-define( 'ASTRA_THEME_URI', trailingslashit( esc_url( get_template_directory_uri() ) ) );
+define('ASTRA_THEME_VERSION', '3.7.5');
+define('ASTRA_THEME_SETTINGS', 'astra-settings');
+define('ASTRA_THEME_DIR', trailingslashit(get_template_directory()));
+define('ASTRA_THEME_URI', trailingslashit(esc_url(get_template_directory_uri())));
 
 
 /**
  * Minimum Version requirement of the Astra Pro addon.
  * This constant will be used to display the notice asking user to update the Astra addon to the version defined below.
  */
-define( 'ASTRA_EXT_MIN_VER', '3.6.3' );
+define('ASTRA_EXT_MIN_VER', '3.6.3');
 
 /**
  * Setup helper functions of Astra.
@@ -48,7 +49,7 @@ require_once ASTRA_THEME_DIR . 'inc/theme-update/class-astra-pb-compatibility.ph
  * Fonts Files
  */
 require_once ASTRA_THEME_DIR . 'inc/customizer/class-astra-font-families.php';
-if ( is_admin() ) {
+if (is_admin()) {
 	require_once ASTRA_THEME_DIR . 'inc/customizer/class-astra-fonts-data.php';
 }
 
@@ -104,14 +105,13 @@ require_once ASTRA_THEME_DIR . 'inc/core/class-astra-admin-helper.php';
 
 require_once ASTRA_THEME_DIR . 'inc/schema/class-astra-schema.php';
 
-if ( is_admin() ) {
+if (is_admin()) {
 
 	/**
 	 * Admin Menu Settings
 	 */
 	require_once ASTRA_THEME_DIR . 'inc/core/class-astra-admin-settings.php';
 	require_once ASTRA_THEME_DIR . 'inc/lib/astra-notices/class-astra-notices.php';
-
 }
 
 /**
@@ -157,14 +157,14 @@ require_once ASTRA_THEME_DIR . 'inc/addons/heading-colors/class-astra-heading-co
 require_once ASTRA_THEME_DIR . 'inc/builder/class-astra-builder-loader.php';
 
 // Elementor Compatibility requires PHP 5.4 for namespaces.
-if ( version_compare( PHP_VERSION, '5.4', '>=' ) ) {
+if (version_compare(PHP_VERSION, '5.4', '>=')) {
 	require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-elementor.php';
 	require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-elementor-pro.php';
 	require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-web-stories.php';
 }
 
 // Beaver Themer compatibility requires PHP 5.3 for anonymus functions.
-if ( version_compare( PHP_VERSION, '5.3', '>=' ) ) {
+if (version_compare(PHP_VERSION, '5.3', '>=')) {
 	require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-beaver-themer.php';
 }
 
